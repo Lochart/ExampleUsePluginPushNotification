78ec4b8f4a94bdd4e70600898752ca922f07261f
Nikolay <dmc66609@mail.ru>
2019-06-27 10:18:40 +0500

    Fisrt commit
    Clone exapmle project Plugin.PushNotification;
    Replace file .json only on Android

---
+++ PushNotificationSample.Android/obj/Debug/android/src/mono/android/app/ApplicationRegistration.java
@@ -0,0 +1,11 @@
+package mono.android.app;
+
+public class ApplicationRegistration {
+
+	public static void registerApplications ()
+	{
+				// Application and Instrumentation ACWs must be registered first.
+		mono.android.Runtime.register ("PushNotificationSample.Droid.MainApplication, PushNotificationSample.Android, Version=1.0.0.0, Culture=neutral, PublicKeyToken=null", md503af76d6218102b2b3334001f41da5f3.MainApplication.class, md503af76d6218102b2b3334001f41da5f3.MainApplication.__md_methods);
+		
+	}
+}
---
+++ PushNotificationSample.Android/obj/Debug/android/src/mono/android/app/XamarinAndroidEnvironmentVariables.java
@@ -0,0 +1,14 @@
+package mono.android.app;
+
+public class XamarinAndroidEnvironmentVariables
+{
+	// Variables are specified the in "name", "value" pairs
+	public static final String[] Variables = new String[] {
+		"MONO_LOG_LEVEL", "info",
+		"XAMARIN_BUILD_ID", "792c0059-5c40-4d71-a0d8-e4e6e8c38f02",
+		"XA_HTTP_CLIENT_HANDLER_TYPE", "System.Net.Http.HttpClientHandler, System.Net.Http",
+		"XA_TLS_PROVIDER", "btls",
+		"MONO_GC_PARAMS", "major=marksweep",
+
+	};
+}
